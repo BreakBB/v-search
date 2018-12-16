b722c5441ab62b9f73651b890373121ce30bef05
BreakBB <b.boeing@online.de>
2018-12-16 11:14:43 +0100

    Updates

---
+++ precache-manifest.0c1392fb766c6220cabf17c4757cc43d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33642b2ad8ba6408bd20",
+    "url": "/v-search/static/js/runtime~main.33642b2a.js"
+  },
+  {
+    "revision": "8671ad7ba5f17608d842",
+    "url": "/v-search/static/js/main.8671ad7b.chunk.js"
+  },
+  {
+    "revision": "37aa85af84d9bb3f2291",
+    "url": "/v-search/static/js/1.37aa85af.chunk.js"
+  },
+  {
+    "revision": "8671ad7ba5f17608d842",
+    "url": "/v-search/static/css/main.6f140d0c.chunk.css"
+  },
+  {
+    "revision": "0c37b7be63c67e2e0862f1225bcb5970",
+    "url": "/v-search/index.html"
+  }
+];
