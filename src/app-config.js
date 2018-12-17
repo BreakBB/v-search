4037ab3322e80afbadc870d6270725f2e481c0a3
BreakBB <b.boeing@online.de>
2018-12-17 12:26:14 +0100

    Update app-config to use localhost as port when working in dev

--- src/app-config.js
+++ src/app-config.js
@@ -1,6 +1,10 @@
 export const LOGIN_PATH = process.env.PUBLIC_URL + "/Login";
 export const HOME_PATH = process.env.PUBLIC_URL + "/";
 
-const BACKEND_URL = "http://ec2-18-184-61-80.eu-central-1.compute.amazonaws.com";
+let BACKEND_URL = "http://ec2-18-184-61-80.eu-central-1.compute.amazonaws.com";
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+  BACKEND_URL = "http://localhost"
+}
 const BACKEND_PORT = "8666";
 export const BACKEND_ADDRESS = BACKEND_URL + ":" + BACKEND_PORT;
+console.log("Backend location is: ", BACKEND_ADDRESS);
