06cf3b487d00a6d8261096b46742c6af3456f466
BreakBB <b.boeing@online.de>
2019-01-21 12:47:34 +0100

    Add login_token to localStorage to make logins persistent during page reloads

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -4,13 +4,44 @@ class AuthStore {
   isLoggedIn = false;
   userId = null;
 
+  constructor() {
+    const login = window.localStorage.getItem('login_token');
+
+    if (login != null) {
+      const data = JSON.parse(login);
+      console.log("login_token:", data);
+      // Login should expire after 10 minutes
+      if (Date.now() - data.timestamp < 60000) {
+        this.isLoggedIn = true;
+        this.userId = data.userId;
+        this.refreshTokenTime();
+      }
+      else {
+        this.removeToken();
+      }
+    }
+  }
+
   login(userId) {
     this.isLoggedIn = true;
     this.userId = userId;
+    this.refreshTokenTime();
   }
 
   logout() {
-    this.isLoggedIn = false
+    this.isLoggedIn = false;
+    this.removeToken();
+  }
+
+  refreshTokenTime() {
+    window.localStorage.setItem('login_token', JSON.stringify({
+      "timestamp": Date.now(),
+      "userId": this.userId
+    }));
+  }
+
+  removeToken() {
+    window.localStorage.removeItem('login_token');
   }
 }
 
